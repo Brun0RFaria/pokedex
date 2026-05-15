e35665fabbcc1d9f249b9f7247c1c6b54e2f9406
Brun0RFaria <brunoraimundofaria@gmail.com>
2026-05-14 21:49:35 -0300

    adicionada lista filtrada

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,13 +1,61 @@
 import PokemonCard from './PokemonCard'
+import { useState } from 'react'
+
+const POKEMONS = [
+   {
+     id: 25,
+     name: 'Pikachu',
+     type: 'Elétrico',
+     imageUrl:
+       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+   },
+  {
+     id: 1,
+     name: 'Bulbasaur',
+     type: 'Grama / Veneno',
+     imageUrl:
+     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+   },
+]
+
 
 function PokemonList() {
-  return (
-    <section>
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-    </section>
+
+  const [pokemons] = useState(POKEMONS)
+  const [filtro, setFiltro] = useState('')
+
+  const listaFiltrada = pokemons.filter((p) =>
+  p.name.toLowerCase().includes(filtro.toLowerCase())
   )
+
+
+   return (
+     <section>
+     <label htmlFor="busca">Buscar por nome: </label>
+     <input
+       id="busca"
+       type="search"
+       value={filtro}
+       onChange={(e) => setFiltro(e.target.value)}
+       placeholder="Ex.: char"/>
+
+      
+      {listaFiltrada.length === 0 ?(
+        <label>Nenhum pokemon encontrado</label>
+       ):(
+        <label>Mostrando {listaFiltrada.length} pokemons:</label>
+      )}
+
+     {listaFiltrada.map((pokemon) => (
+       <PokemonCard 
+         key={pokemon.id}
+         id={pokemon.id}
+         name={pokemon.name}
+         type={pokemon.type}
+         imageUrl={pokemon.imageUrl} />
+       ))}
+   </section>
+ )
 }
 
 export default PokemonList
